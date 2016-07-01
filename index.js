3cd6ca2adaecc6c20be474b959d9959018d9cc66
Dao Lam <daolam112@gmail.com>
2016-06-30 21:12:49 -0700

    Add function to stream logs

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ const path = require('path');
 const request = require('request');
 const tinytim = require('tinytim');
 const yaml = require('js-yaml');
+const hoek = require('hoek');
 const API_KEY = fs.readFileSync('/etc/kubernetes/apikey/token', 'utf-8');
 const SCM_URL_REGEX = /^git@([^:]+):([^\/]+)\/(.+?)\.git(#.+)?$/;
 const GIT_ORG = 2;
 const GIT_REPO = 3;
 const GIT_BRANCH = 4;
 const jobsUrl = 'https://kubernetes/apis/batch/v1/namespaces/default/jobs';
+const podsUrl = 'https://kubernetes/apis/v1/namespaces/default/pods';
 
 class K8sExecutor extends Executor {
     /**
      * Starts a k8s build
      * @method start
      * @param  {Object}   config            A configuration object
-     * @param  {Object}   config.buildId    ID for the build
-     * @param  {Object}   config.jobId      ID for the job
-     * @param  {Object}   config.pipelineId ID for the pipeline
-     * @param  {Object}   config.container  Container for the build to run in
-     * @param  {Object}   config.scmUrl     Scm URL to use in the build
-     * @param  {Function} callback Callback function
+     * @param  {String}   config.buildId    ID for the build
+     * @param  {String}   config.jobId      ID for the job
+     * @param  {String}   config.pipelineId ID for the pipeline
+     * @param  {String}   config.container  Container for the build to run in
+     * @param  {String}   config.scmUrl     Scm URL to use in the build
+     * @param  {Function} callback          Callback function
      */
     start(config, callback) {
         const scmMatch = SCM_URL_REGEX.exec(config.scmUrl);
@@ -56,6 +58,38 @@ class K8sExecutor extends Executor {
             }
 
             return callback(null);
+        });
+    }
+
+    /**
+    * Streams logs
+    * @method stream
+    * @param  {Object}   config            A configuration object
+    * @param  {String}   config.buildId    ID for the build
+    * @param  {Response} response          Response object to stream logs
+    */
+    stream(config, response) {
+        const pod = `${podsUrl}?labelSelector=sdbuild=${config.buildId}`;
+        const options = {
+            headers: {
+                Authorization: `Bearer ${API_KEY}`
+            },
+            json: true,
+            strictSSL: false
+        };
+
+        request.get(pod, options, (err, resp, body) => {
+            if (err) {
+                return response(new Error(`Error getting pod with sdbuild=${config.buildId}`));
+            }
+            const podName = hoek.reach(body, 'items.0.metadata.name');
+
+            if (!podName) {
+                return response(new Error('Error getting pod name'));
+            }
+            const logUrl = `${podsUrl}/${podName}/log?container=build&follow=true&pretty=true`;
+
+            return request.get(logUrl).pipe(response);
         });
     }
 }
