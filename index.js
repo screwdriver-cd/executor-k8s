a14b4de9ef4300d00dd84bf6c14c4398b03d7143
Lakshminarasimhan Parthasarathy <laky@ymail.com>
2020-08-17 14:20:08 -0700

    fix: add cache max go threads config (#131)
    Co-authored-by: parthasl <parthasl@verizonmedia.com>

--- index.js
+++ index.js
@@ -192,6 +192,7 @@ class K8sExecutor extends Executor {
      * @param  {String}  [options.ecosystem.cache.compress=false]                Value for build cache compress - true / false; used only when cache.strategy is disk
      * @param  {String}  [options.ecosystem.cache.md5check=false]                Value for build cache md5check - true / false; used only when cache.strategy is disk
      * @param  {String}  [options.ecosystem.cache.max_size_mb=0]                 Value for build cache max size in mb; used only when cache.strategy is disk
+     * @param  {String}  [options.ecosystem.cache.max_go_threads=10000]          Value for build cache max go threads; used only when cache.strategy is disk
      */
     constructor(options = {}) {
         super();
@@ -244,6 +245,8 @@ class K8sExecutor extends Executor {
         this.cacheMd5Check = hoek.reach(options, 'ecosystem.cache.md5check', { default: 'false' });
         this.cacheMaxSizeInMB = hoek.reach(options,
             'ecosystem.cache.max_size_mb', { default: 0 });
+        this.cacheMaxGoThreads = hoek.reach(options,
+            'ecosystem.cache.max_go_threads', { default: 10000 });
         this.dockerFeatureEnabled = hoek.reach(options, 'kubernetes.dockerFeatureEnabled',
             { default: false });
         this.annotations = hoek.reach(options, 'kubernetes.annotations');
@@ -430,6 +433,7 @@ class K8sExecutor extends Executor {
                 compress: this.cacheCompress,
                 md5check: this.cacheMd5Check,
                 max_size_mb: this.cacheMaxSizeInMB,
+                max_go_threads: this.cacheMaxGoThreads,
                 volumeReadOnly
             },
             service_account: this.serviceAccount,
