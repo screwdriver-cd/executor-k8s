d696c40774702d36e6a188409eec85bbc0249013
Mansoor C <mansoorchengat@yahoo.com>
2021-07-20 16:02:25 +0530

    adding ssh secret to pod template

--- index.js
+++ index.js
@@ -214,6 +214,8 @@ class K8sExecutor extends Executor {
      * @param  {String}  [options.ecosystem.cache.md5check=false]                Value for build cache md5check - true / false; used only when cache.strategy is disk
      * @param  {String}  [options.ecosystem.cache.max_size_mb=0]                 Value for build cache max size in mb; used only when cache.strategy is disk
      * @param  {String}  [options.ecosystem.cache.max_go_threads=10000]          Value for build cache max go threads; used only when cache.strategy is disk
+     * @param  {Object}  [options.kubernetes.secrets]                            Object representing secrets (e.g.: [ { "env_name": "SSHCA_PRIVATE_KEY", "key_name": "sshca-secret", "key", "private" } ] )
+
      */
     constructor(options = {}) {
         super();
@@ -273,6 +275,7 @@ class K8sExecutor extends Executor {
         this.dockerFeatureEnabled = hoek.reach(options, 'kubernetes.dockerFeatureEnabled', { default: false });
         this.annotations = hoek.reach(options, 'kubernetes.annotations');
         this.privileged = hoek.reach(options, 'kubernetes.privileged', { default: false });
+        this.secrets = hoek.reach(options, 'kubernetes.secrets', { default: {} });
         this.scheduleStatusRetryStrategy = (err, response, body) => {
             const conditions = hoek.reach(body, 'status.conditions');
             let scheduled = false;
@@ -563,7 +566,8 @@ class K8sExecutor extends Executor {
             },
             dns_policy: this.dnsPolicy,
             image_pull_policy: this.imagePullPolicy,
-            volume_mounts: this.volumeMounts
+            volume_mounts: this.volumeMounts,
+            secret_entity: this.secrets
         });
         const podConfig = yaml.safeLoad(podTemplate);
         const nodeSelectors = {};
