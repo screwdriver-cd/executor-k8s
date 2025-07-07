08b553e6c099d558ed4c79130c2244d5b8b56b35
Teppei Minegishi <tminegis@lycorp.co.jp>
2025-07-08 08:45:38 +0900

    fix

--- index.js
+++ index.js
@@ -347,6 +347,7 @@ class K8sExecutor extends Executor {
         this.cacheMaxSizeInMB = hoek.reach(options, 'ecosystem.cache.max_size_mb', { default: 0 });
         this.cacheMaxGoThreads = hoek.reach(options, 'ecosystem.cache.max_go_threads', { default: 10000 });
         this.dockerFeatureEnabled = hoek.reach(options, 'kubernetes.dockerFeatureEnabled', { default: false });
+        this.rootlessBuildkitFeatureEnabled = hoek.reach(options, 'kubernetes.rootlessBuildkitFeatureEnabled', { default: false });
         this.annotations = hoek.reach(options, 'kubernetes.annotations');
         this.privileged = hoek.reach(options, 'kubernetes.privileged', { default: false });
         this.secrets = hoek.reach(options, 'kubernetes.buildSecrets', { default: {} });
